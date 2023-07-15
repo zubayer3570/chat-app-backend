f0ec59a1afa80e4130c5f37593b7519b9a96c8ae
zubayer <zubayer3570@gmail.com>
2023-07-15 23:21:05 +0600

    hello

--- Contorllers/people.controller.js
+++ Contorllers/people.controller.js
@@ -15,7 +15,8 @@ const signupController = async (req, res) => {
         const userData = { ...req.body, profileImg: cloudinaryResponse.secure_url, _id, conversationIDs: [] }
         const newUser = new User(userData)
         const response = await newUser.save()
-        await io.emit("new_user", { ...response, active: true })
+        response.active = true
+        await io.emit("new_user", response)
         res.send(response)
     } catch (error) {
         res.send(error)
