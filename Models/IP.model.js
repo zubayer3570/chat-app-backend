2af9352ee83dac2ef54c1f1c6aeac9af70fa8ba5
zubayer <zubayer3570@gmail.com>
2023-07-30 04:12:21 +0600

    hi

--- Models/IP.model.js
+++ Models/IP.model.js
@@ -2,5 +2,5 @@ const mongoose = require("mongoose")
 const ipSchema = new mongoose.Schema({
     ip: String
 }, { timestamps: true })
-const IpModel = mongoose.model("people", ipSchema)
+const IpModel = mongoose.model("ip", ipSchema)
 module.exports = IpModel
